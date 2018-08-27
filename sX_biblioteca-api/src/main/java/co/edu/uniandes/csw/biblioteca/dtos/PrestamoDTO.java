3288706b4989ac7e25e302a7211df459e7e03e19
estudiante <estudiante@ISIS2603-371.sis.virtual.uniandes.edu.co>
2018-08-27 09:20:58 -0500

    Paquete DTO

--- sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/dtos/PrestamoDTO.java
+++ sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/dtos/PrestamoDTO.java
@@ -3,13 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.edu.uniandes.csw.biblioteca.resources;
+package co.edu.uniandes.csw.biblioteca.dtos;
+
+import co.edu.uniandes.csw.biblioteca.adapters.*;
 
 /**
  *
  * @author estudiante
  */
-public class BibliotecaResource {
-    
+public class PrestamoDTO 
+{
     
 }
