fc5c46b22849df97c9dfb1414aebde8119fbc75f
Nicolas <naav972@gmail.com>
2018-08-27 19:26:44 -0500

    Video DTO gets y sets, Video Resource

--- sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/dtos/VideoDTO.java
+++ sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/dtos/VideoDTO.java
@@ -7,19 +7,67 @@ package co.edu.uniandes.csw.biblioteca.adapters;
 
  public class VideoDTO {
 
-   private String id;
+   private Long id;
    private String nombre;
    private String director;
-   private int unidadesDisponibles;
+   private Integer unidadesDisponibles;
    private String idioma;
-   private boolean subtitulos;
+   private Boolean subtitulos;
 
-   public VideoDTO(String pID, String pNombre, String pDirector, int pUnidades, String pIdioma, boolean pSubtitulos){
+   public VideoDTO(Long pID, String pNombre, String pDirector, int pUnidades, String pIdioma, boolean pSubtitulos){
      id = pID;
      nombre = pNombre;
      director = pDirector;
      unidadesDisponibles = pUnidades;
      idioma = pIdioma;
+     subtitulos = pSubtitulos;
+   }
+
+   public Long getId(){
+     return id;
+   }
+
+   public String getNombre(){
+     return nombre;
+   }
+
+   public String getDirector(){
+     return director;
+   }
+
+   public Integer getUnidadesDis(){
+     return unidadesDisponibles;
+   }
+
+   public String getIdioma(){
+     return idioma;
+   }
+
+   public Boolean getSubtitulos(){
+     return subtitulos;
+   }
+
+   public void setId(Long pID){
+     id = pID;
+   }
+
+   public void setNombre(String pNombre){
+     nombre = pNombre;
+   }
+
+   public void setDirector(String pDirector){
+     director = pDirector;
+   }
+
+   public void setUnidadesDis(Integer pUnidades){
+     unidadesDisponibles = pUnidades;
+   }
+
+   public void setIdioma(String pIdioma){
+     idioma = pIdioma;
+   }
+
+   public void setSubtitulos(Boolean pSubtitulos){
      subtitulos = pSubtitulos;
    }
  }
--- sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/resources/VideoResource.java
+++ sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/resources/VideoResource.java
@@ -5,6 +5,24 @@ package co.edu.uniandes.csw.biblioteca.resources;
  * @author Nicolas Alvarado
  */
 
+ import co.edu.uniandes.csw.biblioteca.dtos.VideoDTO;
+ import javax.enterprise.context.RequestScoped;
+ import javax.inject.Inject;
+ import javax.ws.rs.*;
+
+ @Path("videos")
+ @Produces("application/json")
+ @Consumes("application/json")
+ @RequestScoped
+
  public class VideoResource {
-   
+   @POST
+   public VideoDTO createVideo(VideoDTO video){
+     return null;
+   }
+
+   @GET
+   public VideoDTO getVideo(){
+     return null;
+   }
  }
