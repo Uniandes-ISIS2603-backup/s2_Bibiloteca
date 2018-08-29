8625674fa3a4b48487e7468bba6dc8e2be6f1d0a
estudiante <estudiante@ISIS2603-367.sis.virtual.uniandes.edu.co>
2018-08-29 09:30:33 -0500

    Avance Daniel Montoya

--- sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/resources/VideoResource.java
+++ sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/resources/VideoResource.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package co.edu.uniandes.csw.biblioteca.resources;
 
 /**
@@ -26,3 +27,34 @@ package co.edu.uniandes.csw.biblioteca.resources;
      return null;
    }
  }
+=======
+package co.edu.uniandes.csw.biblioteca.resources;
+
+/**
+ *
+ * @author Nicolas Alvarado
+ */
+
+ import co.edu.uniandes.csw.biblioteca.dtos.VideoDTO;
+ import javax.enterprise.context.RequestScoped;
+ import javax.inject.Inject;
+ import javax.ws.rs.*;
+
+ @Path("videos")
+ @Produces("application/json")
+ @Consumes("application/json")
+ @RequestScoped
+
+ public class VideoResource {
+   @POST
+   public VideoDTO createVideo(VideoDTO video){
+     return null;
+   }
+
+   @GET
+   @Path("{videosId: \\d+}")
+   public VideoDTO getVideo(@PathParam("videosId") Long videosId){
+     return null;
+   }
+ }
+>>>>>>> 772b6c8aee201e649e0b0c10c41abfc180b7ff9e
