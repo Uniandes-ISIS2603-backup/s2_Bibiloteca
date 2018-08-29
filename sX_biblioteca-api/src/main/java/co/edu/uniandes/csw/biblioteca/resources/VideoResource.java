11f121685b417dfd70870a8f7bd7d32942a9d9cf
Nicolas <naav972@gmail.com>
2018-08-29 16:40:56 -0500

    arreglo clase VideoResource

--- sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/resources/VideoResource.java
+++ sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/resources/VideoResource.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package co.edu.uniandes.csw.biblioteca.resources;
 
 /**
@@ -27,34 +26,3 @@ package co.edu.uniandes.csw.biblioteca.resources;
      return null;
    }
  }
-=======
-package co.edu.uniandes.csw.biblioteca.resources;
-
-/**
- *
- * @author Nicolas Alvarado
- */
-
- import co.edu.uniandes.csw.biblioteca.dtos.VideoDTO;
- import javax.enterprise.context.RequestScoped;
- import javax.inject.Inject;
- import javax.ws.rs.*;
-
- @Path("videos")
- @Produces("application/json")
- @Consumes("application/json")
- @RequestScoped
-
- public class VideoResource {
-   @POST
-   public VideoDTO createVideo(VideoDTO video){
-     return null;
-   }
-
-   @GET
-   @Path("{videosId: \\d+}")
-   public VideoDTO getVideo(@PathParam("videosId") Long videosId){
-     return null;
-   }
- }
->>>>>>> 772b6c8aee201e649e0b0c10c41abfc180b7ff9e
