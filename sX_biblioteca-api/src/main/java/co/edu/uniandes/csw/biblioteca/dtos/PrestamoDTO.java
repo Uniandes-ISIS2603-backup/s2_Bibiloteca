772b6c8aee201e649e0b0c10c41abfc180b7ff9e
estudiante <estudiante@ISIS2603-371.sis.virtual.uniandes.edu.co>
2018-08-29 09:13:01 -0500

    Prestamo DTO y PrestamoResource

--- sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/dtos/PrestamoDTO.java
+++ sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/dtos/PrestamoDTO.java
@@ -6,6 +6,7 @@
 package co.edu.uniandes.csw.biblioteca.dtos;
 
 import co.edu.uniandes.csw.biblioteca.adapters.*;
+import java.sql.Date;
 
 /**
  *
@@ -13,5 +14,93 @@ import co.edu.uniandes.csw.biblioteca.adapters.*;
  */
 public class PrestamoDTO 
 {
+    //--------------------------------------------
+    // ATRIBUTOS
+    //--------------------------------------------
     
+    /**
+     * Se refiera a la fecha en la que se presta el libro
+     */
+    private Date fechaDeSalida;
+   
+    /**
+     * Se refiera a la fecha en la que se debe entregar el libro
+     */
+    private Date fechaDeEntrega;
+    
+    /**
+     * Se refiere a si el libro fue devuelto o no
+     */ 
+    private boolean retornado;
+    
+    
+    //----------------------------------------------
+    // CONSTRUCTOR
+    //----------------------------------------------
+    
+    public PrestamoDTO()
+    {
+        
+    }
+    
+    //----------------------------------------------
+    // METODOS
+    //----------------------------------------------
+    
+    /**
+     * Método para cambiar la fecha de salida
+     * @param pFechaDeSalida Nueva fecha de salida
+     */
+    public void setFechaDeSalida(Date pFechaDeSalida)
+    {
+        this.fechaDeSalida= pFechaDeSalida;
+    }
+    
+    /**
+     * Método que da la fecha de salida del libro
+     * @return Fecha De salida
+     */
+    public Date getFechaDeSalida()
+    {
+        return fechaDeSalida;
+    }
+    
+    /**
+     * Método para cambiar la fecha de entrega
+     * @param pFechaDeEntrega Nueva fecha de entrega
+     */
+    public void setFechaDeEntrega(Date pFechaDeEntrega)
+    {
+        this.fechaDeSalida= pFechaDeEntrega;
+    }
+    
+    /**
+     * Método que da la fecha de entrega del libro
+     * @return Fecha De entrega
+     */
+    public Date getFechaDeEntrega()
+    {
+        return fechaDeEntrega;
+    }
+
+    /**
+     * Método para cambiar si el libro ha sido retornado o no
+     * @param pRetornado Booleano con el valor para retornado
+     */
+    public void setRetornado (boolean pRetornado)
+    {
+        retornado = pRetornado;
+    }
+    
+    /**
+     * Devuelve si el libro ha sido retornado o no
+     * @return  true: si el libro ha sido retornado
+     *          false: si el libro no ha sido retornado
+     */
+    public boolean darRetornado()
+    {
+        return retornado;
+    }
+    
+   
 }
--- sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/resources/PrestamoResource.java
+++ sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/resources/PrestamoResource.java
@@ -5,10 +5,33 @@
  */
 package co.edu.uniandes.csw.biblioteca.resources;
 
+
+import java.util.logging.Level;
+import javax.ws.rs.*;
+import co.edu.uniandes.csw.biblioteca.dtos.PrestamoDTO;
+import javax.enterprise.context.RequestScoped;
 /**
+ * Clase que implementa el recurso "prestamo".
  *
- * @author estudiante
+ * @author David Saavedra
+ * @version 1.0
  */
-public class PrestamoResource {
+@Path("prestamos")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
+public class PrestamoResource 
+{
+     @POST
+    public PrestamoDTO createBiblioteca(PrestamoDTO prestamo) 
+    {
+       return prestamo;
+    }
     
+    @GET
+    @Path("{bibliotecasId: \\d+}")
+    public PrestamoDTO getBiblioteca(@PathParam("bibliotecasId") Long librosId)
+    {
+       return null; 
+    }
 }
