e9f68278cb5e672c14703f907a1f8ca27ed8e603
estudiante <estudiante@ISIS2603-356.sis.virtual.uniandes.edu.co>
2018-08-29 22:15:02 -0500

    DTO y Resource

---
+++ sX_biblioteca-api/src/main/java/co/edu/uniandes/csw/biblioteca/resources/UsuarioResource.java
@@ -0,0 +1,36 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.biblioteca.resources;
+import javax.ws.rs.*;
+import javax.enterprise.context.RequestScoped;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+/**
+ *
+ * @author Juan Nicolás García
+ */
+@Path("usarios")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
+public class UsuarioResource {
+    @POST
+    public UsuarioDTO createUsuario(UsuarioDTO usuario)
+    {
+        return usuario;
+    }
+    @GET
+    @Path("{usuariosId: \\d+}")
+    public UsuarioDTO getUsuario(@PathParam("usuariosId") Long usuariosId)
+    {
+        return null;
+    }
+    
+}
