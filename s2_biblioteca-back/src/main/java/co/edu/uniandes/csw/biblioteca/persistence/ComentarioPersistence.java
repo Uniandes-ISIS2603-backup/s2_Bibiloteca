07c3413afc4fc0a383ff03e34f9d00b762daaf91
estudiante <estudiante@ISIS2603-367.sis.virtual.uniandes.edu.co>
2018-09-03 07:47:44 -0500

    Persistencia terminada, faltan pruebas

---
+++ s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/biblioteca/persistence/ComentarioPersistence.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.biblioteca.persistence;
+
+import co.edu.uniandes.csw.bibilioteca.entities.ComentarioEntity;
+import co.edu.uniandes.csw.bibilioteca.entities.LibroEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author estudiante
+ */
+public class ComentarioPersistence {
+        
+    private static final Logger LOGGER = Logger.getLogger(LibroPersistence.class.getName());
+    
+    @PersistenceContext(unitName = "BibliotecaPU")
+    protected EntityManager em;
+    
+    
+    public ComentarioEntity create(ComentarioEntity comentario)
+    {
+        LOGGER.log(Level.INFO,"Creando comentario nuevo");
+        em.persist(comentario);
+        LOGGER.log(Level.INFO,"comentario creado");
+        return comentario;
+    }
+    
+    public ComentarioEntity find (Long libroId,Long comentarioId)
+    {
+        LOGGER.log(Level.INFO,"Consultando comentario con id={0} del libro con id = "+libroId,comentarioId);
+        TypedQuery q = em.createQuery("select p from ComentarioEntity p where (p.libro.id = :libroId) and (p.id = :comentarioId)", ComentarioEntity.class);
+        q.setParameter("libroId", libroId);
+        q.setParameter("comentarioId", comentarioId);
+        List<ComentarioEntity> resultado = q.getResultList();
+        ComentarioEntity comentario = null;
+        if(resultado == null)
+        {
+            comentario = null;
+        }
+        else if(resultado.isEmpty())
+        {
+            comentario = null;
+        }
+        else
+        {
+            comentario = resultado.get(0);
+        }
+        LOGGER.log(Level.INFO, "Saliendo de consultar el comentario con id = {0} del libro con id =" + libroId  , comentarioId);
+        return comentario;
+    }
+    
+    public ComentarioEntity update(ComentarioEntity comentario)
+    {
+        LOGGER.log(Level.INFO,"Actualiazando comentario con id={0}",comentario.getId());
+        return em.merge(comentario);
+    }
+    
+    public void delete(Long comentarioId)
+    {
+        LOGGER.log(Level.INFO,"Borrando libro con id={0}",comentarioId);
+        ComentarioEntity comentario  = em.find(ComentarioEntity.class, comentarioId);
+        em.remove(comentario);
+        LOGGER.log(Level.INFO,"Elimanando comentario con id={0}",comentarioId);
+    }
+    
+}
--- s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/biblioteca/persistence/LibroPersistence.java
+++ s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/biblioteca/persistence/LibroPersistence.java
@@ -6,11 +6,14 @@
 package co.edu.uniandes.csw.biblioteca.persistence;
 
 import co.edu.uniandes.csw.bibilioteca.entities.LibroEntity;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -25,11 +28,65 @@ public class LibroPersistence {
     protected EntityManager em;
     
     
-    public LibroEntity createLibro(LibroEntity libro)
+    public LibroEntity create(LibroEntity libro)
     {
         LOGGER.log(Level.INFO,"Creando libro nuevo");
         em.persist(libro);
         LOGGER.log(Level.INFO,"Libro creado");
         return libro;
+    }
+    
+    public List<LibroEntity> findAll()
+    {
+        LOGGER.log(Level.INFO,"Consultando todos los libros");
+        Query q = em.createQuery("select u from LibroEntity u");
+        return q.getResultList();
+    }
+    
+    public LibroEntity find (Long libroId)
+    {
+        LOGGER.log(Level.INFO,"Consultando libro con id={0}",libroId);
+        return em.find(LibroEntity.class, libroId);
+    }
+    
+    public LibroEntity update(LibroEntity libroEntity)
+    {
+        LOGGER.log(Level.INFO,"Actualiazando libro con id={0}",libroEntity.getId());
+        return em.merge(libroEntity);
+    }
+    
+    public void delete(Long libroId)
+    {
+        LOGGER.log(Level.INFO,"Borrando libro con id={0}",libroId);
+        LibroEntity libro = em.find(LibroEntity.class, libroId);
+        em.remove(libro);
+    }
+    
+    public LibroEntity findISBN(String isbn)
+    {
+        LOGGER.log(Level.INFO,"Consultando libros con el ISBN",isbn);
+        
+        TypedQuery query = em.createQuery("Select e From LibroEntity e where e.isbn = :isbn", LibroEntity.class);
+        
+        query = query.setParameter("isbn", isbn);
+        
+        List<LibroEntity> librosConMismoISBN = query.getResultList();
+        
+        LibroEntity result;
+        
+        if(librosConMismoISBN == null)
+        {
+            result = null;
+        }
+        else if(librosConMismoISBN.isEmpty())
+        {
+            result = null;
+        }
+        else
+        {
+            result = librosConMismoISBN.get(0);
+        }
+        LOGGER.log(Level.INFO,"Saliendo de consultar libros por isbn ", isbn);
+        return result;
     }
 }
