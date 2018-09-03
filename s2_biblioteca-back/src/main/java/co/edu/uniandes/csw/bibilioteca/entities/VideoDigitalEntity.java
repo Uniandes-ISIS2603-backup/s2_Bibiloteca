806d215892a4b74c83408ad3b5122d0e5a855847
jsbautista <js.bautista>
2018-09-03 08:49:08 -0500

    entity y persistence VideoDigital

---
+++ s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/bibilioteca/entities/VideoDigitalEntity.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.bibilioteca.entities;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.ManyToOne;
+import uk.co.jemos.podam.common.PodamExclude;
+
+/**
+ *
+ * @author Juan Bautista
+ */
+public class VideoDigitalEntity extends BaseEntity implements Serializable {
+    private Long id;
+    private String nombre;
+    private String director;
+    private String idioma;
+    private Boolean subtitulos;
+    
+    @PodamExclude
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private VideoDigitalEntity videodigital;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public Boolean getSubtitulos() {
+        return subtitulos;
+    }
+
+    public void setSubtitulos(Boolean subtitulos) {
+        this.subtitulos = subtitulos;
+    }
+
+    public VideoDigitalEntity getVideodigital() {
+        return videodigital;
+    }
+
+    public void setVideodigital(VideoDigitalEntity videodigital) {
+        this.videodigital = videodigital;
+    }
+    
+}
