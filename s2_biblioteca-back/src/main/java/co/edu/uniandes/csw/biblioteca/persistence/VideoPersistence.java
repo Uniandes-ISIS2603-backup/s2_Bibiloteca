7599b668e9f4657537585d0188174ede59205085
Nicolas <na.alvarado@uniandes.edu.co>
2018-09-03 17:46:48 -0500

    VideoPersistence

---
+++ s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/biblioteca/persistence/VideoPersistence.java
@@ -0,0 +1,36 @@
+
+package co.edu.uniandes.csw.biblioteca.persistence;
+
+import co.edu.uniandes.csw.bibilioteca.entities.VideoEntity;
+import javax.ejb.Stateless;
+import javax.persistence.*;
+
+/**
+ *
+ * @author Nicolas Alvarado
+ */
+
+@Stateless
+public class VideoPersistence {
+    
+    @PersistenceContext(unitName="dnsDB")
+    protected EntityManager em;
+    
+    public VideoEntity create(VideoEntity video){
+        em.persist(video);
+        return video;
+    }
+    
+    public VideoEntity find(Long videoID){
+        return em.find(VideoEntity.class, videoID);
+    }
+    
+    public VideoEntity update(VideoEntity video){
+        return em.merge(video);
+    }
+    
+    public void delete(Long videoID){
+        VideoEntity v = find(videoID);
+        em.remove(v);
+    }
+}
