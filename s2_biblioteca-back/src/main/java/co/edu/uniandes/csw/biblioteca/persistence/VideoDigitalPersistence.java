a5f51ee8208aacaa5fe80a2a79b53f7352706bda
desaavedra <de.saavedra@uniandes.edu.co>
2018-09-09 15:52:33 -0500

    Prestamo Persistencia

--- s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/biblioteca/persistence/VideoDigitalPersistence.java
+++ s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/biblioteca/persistence/VideoDigitalPersistence.java
@@ -18,7 +18,7 @@ import javax.persistence.PersistenceContext;
  */
 
     @Stateless
-public class VideoDigitalPersistence {
+    public class VideoDigitalPersistence {
     
     private static final Logger LOGGER = Logger.getLogger(VideoDigitalPersistence.class.getName());
     
