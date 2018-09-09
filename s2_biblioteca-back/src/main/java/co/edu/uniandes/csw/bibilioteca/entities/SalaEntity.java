899af0bcaec99a4343a5baf100b858f34b6f0f12
estudiante <estudiante@ISIS2603-356.sis.virtual.uniandes.edu.co>
2018-09-09 11:44:30 -0500

    Sala y UsuarioEntity

--- s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/bibilioteca/entities/SalaEntity.java
+++ s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/bibilioteca/entities/SalaEntity.java
@@ -5,10 +5,40 @@
  */
 package co.edu.uniandes.csw.bibilioteca.entities;
 
+import java.io.Serializable;
+
 /**
  *
- * @author estudiante
+ * @author Juan Nicolás García
  */
-public class SalaEntity {
+public class SalaEntity extends BaseEntity implements Serializable{
+    private String ubicacion;
+    private Boolean disponibilidad;
+    private Integer capacidad;
+    
+    public String getUbicacion()
+    {
+        return ubicacion;
+    }
+    public Boolean getDisponibilidad()
+    {
+        return disponibilidad;
+    }
+    public Integer getCapacidad()
+    {
+        return capacidad;
+    }
+    public void setUbicacion(String ubicacion)
+    {
+        this.ubicacion = ubicacion;
+    }
+    public void setDisponibilidad(Boolean disponibilidad)
+    {
+        this.disponibilidad = disponibilidad;
+    }
+    public void setCapacidad(Integer capacidad)
+    {
+        this.capacidad = capacidad;
+    }
     
 }
