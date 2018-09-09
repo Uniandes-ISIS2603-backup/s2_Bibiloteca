d799b4b659e61a66211213950e527112252b6dd2
desaavedra <de.saavedra@uniandes.edu.co>
2018-09-09 11:44:09 -0500

    PrestamoPersistence 0.2 y correción video digital

--- s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/biblioteca/persistence/VideoDigitalPersistence.java
+++ s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/biblioteca/persistence/VideoDigitalPersistence.java
@@ -22,7 +22,7 @@ public class VideoDigitalPersistence {
     
     private static final Logger LOGGER = Logger.getLogger(VideoDigitalPersistence.class.getName());
     
-    @PersistenceContext(unitName = "dnsDB")
+    @PersistenceContext(unitName = "DnsPU")
     protected EntityManager eman;
     
     
