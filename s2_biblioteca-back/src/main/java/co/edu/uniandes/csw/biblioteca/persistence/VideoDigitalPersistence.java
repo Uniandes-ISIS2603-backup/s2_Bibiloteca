6b2a39365d5c897243a78b8b2394ad6a12ce0a0a
jsbautista <js.bautista>
2018-09-09 20:11:53 -0500

    actualizacion recursos propios

--- s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/biblioteca/persistence/VideoDigitalPersistence.java
+++ s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/biblioteca/persistence/VideoDigitalPersistence.java
@@ -6,11 +6,14 @@
 package co.edu.uniandes.csw.biblioteca.persistence;
 
 import co.edu.uniandes.csw.bibilioteca.entities.VideoDigitalEntity;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -32,6 +35,57 @@ import javax.persistence.PersistenceContext;
         eman.persist(videodigital);
         LOGGER.log(Level.INFO,"Video digital creado");
         return videodigital;
+    }
+    public List <VideoDigitalEntity> findAll()
+    {
+       LOGGER.log(Level.INFO,"Consultando todos los videos digitales");
+       Query qy=eman.createQuery("select u from VideoDigitalEntity u"); 
+       return qy.getResultList();
+    }
+    
+    public VideoDigitalEntity find(Long videoDigitalId)
+    {
+       LOGGER.log(Level.INFO,"Consultando video digital con id={0}",videoDigitalId);
+       return eman.find(VideoDigitalEntity.class,videoDigitalId);
+    }
+      public VideoDigitalEntity findNombre(String nombre)
+    {
+        LOGGER.log(Level.INFO,"Consultando videos digitales con el nombre",nombre);
+        
+        TypedQuery query = eman.createQuery("Select e From VideoDigitalEntity e where e.nombre = :nombre", VideoDigitalEntity.class);
+        
+        query = query.setParameter("nombre", nombre);
+        
+        List<VideoDigitalEntity> videosDigitalesConMismoNombre = query.getResultList();
+        
+        VideoDigitalEntity resultado;
+        
+        if(videosDigitalesConMismoNombre == null)
+        {
+            resultado = null;
+        }
+        else if(videosDigitalesConMismoNombre.isEmpty())
+        {
+            resultado = null;
+        }
+        else
+        {
+            resultado = videosDigitalesConMismoNombre.get(0);
+        }
+        LOGGER.log(Level.INFO,"Saliendo de consultar videos digitales por nombre ", nombre);
+        return resultado;
+    }
+      public VideoDigitalEntity update(VideoDigitalEntity videoDigitalEntity)
+    {
+        LOGGER.log(Level.INFO,"Actualizando video digital con id={0}",videoDigitalEntity.getId());
+        return eman.merge(videoDigitalEntity);
+    }
+    
+    public void delete(Long videoDigitalId)
+    {
+        LOGGER.log(Level.INFO,"Borrando video digital con id={0}",videoDigitalId);
+        VideoDigitalEntity videoDigital = eman.find(VideoDigitalEntity.class, videoDigitalId);
+        eman.remove(videoDigital);
     }
     
 }
