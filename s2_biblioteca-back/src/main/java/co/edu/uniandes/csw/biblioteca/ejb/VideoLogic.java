2cafbfb346322cec0d0fb18d94e99a7f5a0e5efb
Nicolas <na.alvarado@uniandes.edu.co>
2018-09-14 18:20:02 -0500

    parte de videologic

--- s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/biblioteca/ejb/VideoLogic.java
+++ s2_biblioteca-back/src/main/java/co/edu/uniandes/csw/biblioteca/ejb/VideoLogic.java
@@ -1,7 +1,9 @@
 package co.edu.uniandes.csw.biblioteca.ejb;
 
 import co.edu.uniandes.csw.bibilioteca.entities.VideoEntity;
+import co.edu.uniandes.csw.biblioteca.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.biblioteca.persistence.*;
+import java.util.*;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -16,8 +18,28 @@ public class VideoLogic {
     @Inject
     private VideoPersistence videoPersis;
     
-    public VideoEntity createVideo(VideoEntity ve){
-        return null;
+    @Inject
+    private BibliotecaPersistence biblioPersis;
+    
+    public VideoEntity createVideo(VideoEntity ve) throws BusinessLogicException {
+        if(ve.getNombre() == null || ve.getDirector() == null || ve.getIdioma() == null){
+            throw new BusinessLogicException("El nombre, director o idioma no pueden ser nulos");
+        }
+        if(videoPersis.find(ve.getId()) != null || videoPersis.findByName(ve.getNombre()) != null){
+            throw new BusinessLogicException("El video ya existe");
+        }
+        videoPersis.create(ve);
+        return ve;
+    }
+    
+    public List<VideoEntity> getVideos(){
+        List<VideoEntity> listVE = videoPersis.findAll();
+        return listVE;
+    }
+    
+    public VideoEntity getVideo(Long videoID){
+        VideoEntity ve = videoPersis.find(videoID);
+        return ve;
     }
     
 }
