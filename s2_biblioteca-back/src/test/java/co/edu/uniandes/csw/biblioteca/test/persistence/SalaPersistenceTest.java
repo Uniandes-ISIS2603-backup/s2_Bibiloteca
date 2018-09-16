83b61dafa8dd5283f9c46a1882250ad59f109d1d
jngarciae <jn.garciae@uniandes.edu.co>
2018-09-15 23:20:43 -0500

    PersistenceTest

---
+++ s2_biblioteca-back/src/test/java/co/edu/uniandes/csw/biblioteca/test/persistence/SalaPersistenceTest.java
@@ -0,0 +1,158 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.biblioteca.test.persistence;
+
+import co.edu.uniandes.csw.bibilioteca.entities.SalaEntity;
+import co.edu.uniandes.csw.biblioteca.persistence.SalaPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author estudiante
+ */
+public class SalaPersistenceTest {
+      @Inject
+    private SalaPersistence salaPersistence;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    UserTransaction ux;
+    
+    private List<SalaEntity> data = new ArrayList<>();
+    
+        /**
+     * @return Devuelve el jar que Arquillian va a desplegar en Payara embebido.
+     * El jar contiene las clases, el descriptor de la base de datos y el
+     * archivo beans.xml para resolver la inyección de dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(SalaEntity.class.getPackage())
+                .addPackage(SalaPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            ux.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            ux.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                ux.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from SalaEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            SalaEntity entity = factory.manufacturePojo(SalaEntity.class);
+
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+    
+    @Test
+    public void createSalaTest(){
+        PodamFactory factory = new PodamFactoryImpl();
+        SalaEntity newEntity = factory.manufacturePojo(SalaEntity.class);
+        SalaEntity resultado = salaPersistence.create(newEntity);
+        
+        Assert.assertNotNull(resultado);
+        
+        SalaEntity entity = em.find(SalaEntity.class,resultado.getId());
+    
+    }
+    
+    @Test
+    public void getSalaTest(){
+        SalaEntity entity = data.get(0);
+        SalaEntity nuevaEntity = salaPersistence.find(entity.getId());
+        Assert.assertNotNull(nuevaEntity);
+       
+    }
+    
+    @Test
+    public void getSalasTest()
+    {
+        List<SalaEntity> lista = salaPersistence.findAll();
+        Assert.assertEquals(lista.size(), data.size());
+        for(SalaEntity i : lista)
+        {
+            boolean encontrado = false;
+            for(SalaEntity j : data)
+            {
+                if(i.getId().equals(j.getId()))
+                    encontrado = true;
+            }
+            Assert.assertTrue(encontrado);
+        }
+    }
+    
+    @Test
+    public void deleteSalaTest()
+    {
+        SalaEntity entity = data.get(0);
+        salaPersistence.delete(entity.getId());
+        SalaEntity borrado = em.find(SalaEntity.class,entity.getId());
+        Assert.assertNull(borrado);
+    }
+    
+    @Test
+    public void updateSalaTest()
+    {
+        SalaEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        SalaEntity nueva = factory.manufacturePojo(SalaEntity.class);
+        
+        nueva.setId(entity.getId());
+        
+        salaPersistence.update(nueva);
+        
+        SalaEntity resp = em.find(SalaEntity.class, entity.getId());
+        Assert.assertEquals(resp.getId(), nueva.getId());
+        
+    }
+}
---
+++ s2_biblioteca-back/src/test/java/co/edu/uniandes/csw/biblioteca/test/persistence/UsuarioPersistenceTest.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.biblioteca.test.persistence;
+
+import co.edu.uniandes.csw.bibilioteca.entities.UsuarioEntity;
+import co.edu.uniandes.csw.biblioteca.persistence.UsuarioPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author Juan Nicolás García
+ */
+@RunWith(Arquillian.class)
+public class UsuarioPersistenceTest {
+    @Inject
+    private UsuarioPersistence usuarioPersistence;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    UserTransaction ux;
+    
+    private List<UsuarioEntity> data = new ArrayList<>();
+    
+        /**
+     * @return Devuelve el jar que Arquillian va a desplegar en Payara embebido.
+     * El jar contiene las clases, el descriptor de la base de datos y el
+     * archivo beans.xml para resolver la inyección de dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(UsuarioEntity.class.getPackage())
+                .addPackage(UsuarioPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            ux.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            ux.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                ux.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from UsuarioEntity").executeUpdate();
+    }
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            UsuarioEntity entity = factory.manufacturePojo(UsuarioEntity.class);
+
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+    
+    @Test
+    public void createUsuarioTest(){
+        PodamFactory factory = new PodamFactoryImpl();
+        UsuarioEntity newEntity = factory.manufacturePojo(UsuarioEntity.class);
+        UsuarioEntity resultado = usuarioPersistence.create(newEntity);
+        
+        Assert.assertNotNull(resultado);
+        
+        UsuarioEntity entity = em.find(UsuarioEntity.class,resultado.getId());
+        Assert.assertEquals(newEntity.getNombre(), entity.getNombre());
+    }
+    
+    @Test
+    public void getUsuarioTest(){
+        UsuarioEntity entity = data.get(0);
+        UsuarioEntity nuevaEntity = usuarioPersistence.find(entity.getId());
+        Assert.assertNotNull(nuevaEntity);
+        Assert.assertEquals(entity.getNombre(), nuevaEntity.getNombre());
+    }
+    
+    @Test
+    public void getUsuariosTest()
+    {
+        List<UsuarioEntity> lista = usuarioPersistence.findAll();
+        Assert.assertEquals(lista.size(), data.size());
+        for(UsuarioEntity i : lista)
+        {
+            boolean encontrado = false;
+            for(UsuarioEntity j : data)
+            {
+                if(i.getId().equals(j.getId()))
+                    encontrado = true;
+            }
+            Assert.assertTrue(encontrado);
+        }
+    }
+    
+    @Test
+    public void deleteUsuarioTest()
+    {
+        UsuarioEntity entity = data.get(0);
+        usuarioPersistence.delete(entity.getId());
+        UsuarioEntity borrado = em.find(UsuarioEntity.class,entity.getId());
+        Assert.assertNull(borrado);
+    }
+    
+    @Test
+    public void updateUsuarioTest()
+    {
+        UsuarioEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        UsuarioEntity nueva = factory.manufacturePojo(UsuarioEntity.class);
+        
+        nueva.setId(entity.getId());
+        
+        usuarioPersistence.update(nueva);
+        
+        UsuarioEntity resp = em.find(UsuarioEntity.class, entity.getId());
+        Assert.assertEquals(resp.getId(), nueva.getId());
+        Assert.assertEquals(resp.getNombre(), nueva.getNombre());
+    }
+}
